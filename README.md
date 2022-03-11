36426d7fef6646c5cfef66677dd1874c9b7afa78
BOCOVO <bocovojuste@gmail.com>
2022-03-11 10:57:48 +0100

    Add mutation oberver, README.md file

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
+# Welcome to this library
 
+Undraw-js is a library allow you to add [undraw.co](undraw.co) illustration to web page with a specifique color
+
+## How to use it ?
+
+- Add js script to your page
+
+    ```html
+    <script src="https://unpkg.com/undraw-js/0.0.1/index.js"></script>
+    ```
+    This file load the svg file and add the main color you specify
+
+- Add `img` element to you page
+    ```html
+    <img data-ujs-name="Ukraine" data-ujs-color="#f00" />
+    ```
+    About attributes
+    
+    |Options | Description | Required|
+    |--------|-------------|---------|
+    |`data-ujs-name`| Input here the name of the illustration| `yes` |
+    |`data-ujs-color`| Input here the main color code you want| `no` |
+    |`data-ujs-fall-img`|The image link to set as the source of the `img` element if the illustration failed to load.| `no` |
+
+- Add this script to the page
+
+    ```js
+        UndrawJs.init()
+    ```
+    or ( For more customisation )
+
+    ```js
+        const options = {
+            nameAttr: "data-ujs-name", // Custom attribute of nodes that specifies the name of the illustration
+            colorAttr: "data-ujs-color", // Custom attribute of nodes that specifies the main color
+            doneAttr: "data-ujs-done", // Custom attribute of nodes that specifies if the illustration is addded to the node
+            fallbackAttr: "data-ujs-fall-img", // Custom attribute of nodes that specifies the fall image
+            defaultColor: "#f00" // To specify a default color
+        };
+
+        UndrawJs.init(options);
+    ```
+
+## How to find illustration name ? 
+ 
+ Go to [undraw.co](https://undraw.co/illustrations) add illustration you need and copy its name
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,25 +4,36 @@
 
 const prototype = {
     initialConfigs: {
-        nameAttr: "data-ujs-name",
-        colorAttr: "data-ujs-color",
-        doneAttr: "data-ujs-done",
-        fallbackAttr: "data-ujs-fall-img",
+        nameAttr: "data-ujs-name",// Custom attribute of nodes that specifies the name of the illustration
+        colorAttr: "data-ujs-color",// Custom attribute of nodes that specifies the main color
+        doneAttr: "data-ujs-done", // Custom attribute of nodes that specifies if the illustration is addded to the node
+        fallbackAttr: "data-ujs-fall-img", // Custom attribute of nodes that specifies the fall image
         version: "0.0.1"
     },
     maxRetry: 2,
     retryDelay: 3000,
+    initObserver() {
+        if (!this.observed) {
+            const observerCongifs = { childList: true ,subtree:true}
+            const observer = new MutationObserver(() => {
+                this.refresh()
+            })
+            observer.observe(document.body, observerCongifs)
+            this.observed = true // define dom is observed
+        } 
+    },
     init(options = {}) {
         this.options = { ...this.initialConfigs, ...options }
+        this.initObserver()
         this.refresh()
     },
     getUrl(illName) {
         return `https://unpkg.com/undraw-js@${this.options.version}/src/images/${illName}.svg`
     },
-    async loadSvg (url, callBack, fallback, illName, count = 0) {
+    async loadSvg(url, callBack, fallback, illName, count = 0) {
         // load svg from unpkg 
         const errorHandler = () => {
-            if (count < this.maxRetry) { 
+            if (count < this.maxRetry) {
                 setTimeout(() => {
                     this.loadSvg(url, callBack, fallback, illName, count + 1)
                 }, this.retryDelay)
@@ -59,7 +70,7 @@ const prototype = {
         for (let index = 0; index < nodes.length; index++) {
             const node = nodes[index];
             const illName = node.getAttribute(this.options.nameAttr)
-            const color = node.getAttribute(this.options.colorAttr)
+            const color = node.getAttribute(this.options.colorAttr) || this.options.defaultColor
             const fallImage = node.getAttribute(this.options.fallbackAttr)
 
             if (illName) {
@@ -67,6 +78,7 @@ const prototype = {
                 this.loadSvg(url,
                     svg => {
                         node.src = this.getDataImage(svg, color)
+                        node.setAttribute(this.options.doneAttr, "true")
                     },
                     () => {
                         if (!node.getAttribute("alt")) node.setAttribute("alt", illName)
