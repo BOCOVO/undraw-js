92b75f89c620d3bceb6981b568a71c3f7548e795
bocovo <bocovojuste@gmail.com>
2023-12-25 22:06:13 +0100

    chore: remove comments

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,20 +1,4 @@
 module.exports = {
-  /* module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
-  },
-  balel increase the size of bunble to 11.9 KiB
-  */
   mode: "production",
   entry: "./src/index.js",
   output: {
@@ -22,6 +6,6 @@ module.exports = {
     filename: "index.min.js",
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: [".js"],
   },
-}
+};
