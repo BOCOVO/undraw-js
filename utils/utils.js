b30e95cf8d7fa2c0ba63a696d01dcdf9e02052f8
bocovo <bocovojuste@gmail.com>
2023-12-25 20:36:15 +0100

    fix: fix illustrations folder path

--- utils/utils.js
+++ utils/utils.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url"
 export const getIlluFilePath = (name) => {
   // eslint-disable-next-line no-underscore-dangle
   const __dirname = dirname(fileURLToPath(import.meta.url))
-  return resolve(__dirname, `../src/images/${name}.svg`)
+  return resolve(__dirname, `../illus/${name}.svg`)
 }
 
 export const writeIlluFile = async (ill) => {
