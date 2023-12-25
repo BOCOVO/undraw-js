c311fd416c94a36dce4ba59a4b09191cc61885b0
bocovo <bocovojuste@gmail.com>
2023-12-25 22:07:11 +0100

    chore: improve wording

--- utils/utils.js
+++ utils/utils.js
@@ -3,16 +3,16 @@ import fetch from "node-fetch"
 import { dirname, resolve } from "path"
 import { fileURLToPath } from "url"
 
-export const getIlluFilePath = (name) => {
+export const getIllusFilePath = (name) => {
   // eslint-disable-next-line no-underscore-dangle
   const __dirname = dirname(fileURLToPath(import.meta.url))
   return resolve(__dirname, `../illus/${name}.svg`)
 }
 
-export const writeIlluFile = async (ill) => {
+export const writeIllusFile = async (ill) => {
   const response = await fetch(ill.image)
   if (response.ok) {
     const content = await response.text()
-    writeFileSync(getIlluFilePath(ill.title), content)
+    writeFileSync(getIllusFilePath(ill.title), content)
   }
 }
